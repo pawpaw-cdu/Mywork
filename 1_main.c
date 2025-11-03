60db0d637a22fefa634ecba5a8b124568809498d
pawpaw-cdu <2083658933@qq.com>
2025-11-03 16:27:21 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,26 @@
-//
-//
-//
+//202510302124
+//2083658933@qq.com
+//谢金恩
 #include <stdio.h>
-int main()
-{
-    printf("Hello World");
+
+int main(){
+    int i=0,j=0;
+    int size=3;
+    int number[size][size];
+    printf("请输入矩阵:\n");
+    for(i=0;i<size;i++){
+        for(j=0;j<size;j++){
+            scanf("%d",&number[i][j]);
+        }
+    }
+     for(i=0;i<size;i++){
+         for(j=0;j<size;j++){
+             if(j==size-1){
+                 printf("%d\n",number[i][j]);
+             }else{
+                printf("%d ",number[i][j]);
+             }
+         }
+     }
     return 0;
 }
